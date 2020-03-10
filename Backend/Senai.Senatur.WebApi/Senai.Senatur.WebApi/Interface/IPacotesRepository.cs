dedb8b665b3091f6fac51b395e84ff20665ae6be
FIC <FIC@info.local>
2020-03-10 17:11:42 -0300

    Interface pronta de pacotes pronta

--- Backend/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Interface/IPacotesRepository.cs
+++ Backend/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Interface/IPacotesRepository.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using Senai.Senatur.WebApi.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Senai.Senatur.WebApi.Interface
-{
+namespace Senai.Senatur.WebApi.Interface { 
+    // julia fez esse
     interface IPacotesRepository
     {
+    // primeiro tem que colocar o retorno
+    // cadastro é void
+    void Cadastro(Pacotes pacotes);
+    //buscar é Pacotes
+
+    Pacotes Buscar(int id);
+
+    //alterar é void
+    void Alterar(Pacotes pacotes);
+
+    // listar obviamente em listas
+    List<Pacotes> Listar();
+
+    
+
+
+
     }
 }
+
--- Backend/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Repositories/PacotesRepository.cs
+++ Backend/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Repositories/PacotesRepository.cs
@@ -1,11 +1,61 @@
-﻿using System;
+﻿using Senai.Senatur.WebApi.Domains;
+using Senai.Senatur.WebApi.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Senai.Senatur.WebApi.Repositories
+    //julia
+    // famoso crud
 {
-    public class PacotesRepository
+    public class PacotesRepository : IPacotesRepository
     {
+        private SenaturContext ctx = new SenaturContext();
+
+        public void Alterar(Pacotes pacotesAtualizado)
+        {
+            // nesse linha a gente tá procurando o id que a gente fez no corpo da requisição
+            //ele vai procurar no banco os dados
+            //SE ELE ACHAR, ELE VAI
+            //armazenar nesse ctx
+            // e o ctx vai se armazevar no var pacotes
+            //corpo da requisição == isso aqui no body do postman ó:
+            //{
+            //                          "idpacotes" : 1
+            //                       }
+
+            var pacotes = ctx.Pacotes.First(P => P.IdPacote == pacotesAtualizado.IdPacote);
+                                    // esse valor
+            pacotes.NomePacote = pacotesAtualizado.NomePacote;
+            // vem pra cá
+        
+            pacotes.Descricao = pacotesAtualizado.Descricao;
+            pacotes.DataIda = pacotesAtualizado.DataIda;
+            pacotes.DataVolta = pacotesAtualizado.DataVolta;
+            pacotes.Ativo = pacotesAtualizado.Ativo;
+            pacotes.NomeCidade = pacotesAtualizado.NomeCidade;
+
+            // aqui diz que o nome que a gente passou no nome da requisição vai ir pro pacotes
+
+            // agora dando um SaveChanges();
+            ctx.SaveChanges();
+
+                }
+
+        public Pacotes Buscar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastro(Pacotes pacotes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pacotes> Listar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
