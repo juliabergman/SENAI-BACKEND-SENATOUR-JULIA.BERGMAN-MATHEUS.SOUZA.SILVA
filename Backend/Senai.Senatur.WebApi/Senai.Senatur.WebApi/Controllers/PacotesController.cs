f75dfcf3931ec7fbfcee29aaed15204cec05a840
MattSouuza <matheus_ss02@hotmail.com>
2020-03-10 17:26:07 -0300

    Doing Requests

--- Backend/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Controllers/PacotesController.cs
+++ Backend/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Controllers/PacotesController.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Senai.Senatur.WebApi.Domains;
+using Senai.Senatur.WebApi.Interface;
+using Senai.Senatur.WebApi.Repositories;
 
 namespace Senai.Senatur.WebApi.Controllers
 {
+    //Matheus Esteve aq
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class PacotesController : ControllerBase
     {
+        private IPacotesRepository _pacotesRepository { get; set; }
+
+        public PacotesController()
+        {
+            _pacotesRepository = new PacotesRepository();
+        }
+
+        [HttpPut]
+        public IActionResult Put(Pacotes pacotesAtualizado)
+        {
+            //Pacotes pacotesBuscado = _pacotesRepository.Buscar(pacotesAtualizado.IdPacote);
+
+            _pacotesRepository.Alterar(pacotesAtualizado);
+
+            return Ok("Pacote Atualizado.");
+        }
     }
 }
