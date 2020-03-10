25b375de4f235b6e1218167311fcbbb1e901c954
FIC <FIC@info.local>
2020-03-10 14:47:54 -0300

    Colocando as pastas no backend

---
+++ Backend/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Controller/UsuariosController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Senatur.WebApi.Controller
+{
+    public class UsuariosController
+    {
+    }
+}
